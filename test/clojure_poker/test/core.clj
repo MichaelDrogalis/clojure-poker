65947e5d6f023a2199e0f000f3554b73f9e16e9e
Mike Drogalis <mjd3089@rit.edu>
2012-05-20 13:37:03 -0400

    Completed aces test.

--- test/clojure_poker/test/core.clj
+++ test/clojure_poker/test/core.clj
@@ -8,4 +8,10 @@
 
 (deftest deck-has-all-suites-of-aces
   (is
-   (some #{(card-of :ace :diamonds)} deck)))
+   (some #{(card-of :ace :clubs)} deck))
+  (is
+   (some #{(card-of :ace :diamonds)} deck))
+  (is
+   (some #{(card-of :ace :hearts)} deck))
+  (is
+   (some #{(card-of :ace :spades)} deck)))
