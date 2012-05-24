0fa9a09cc4a449e7c702f9dc7c48869e7583d9a2
Michael Drogalis <mjd3089@rit.edu>
2012-05-23 22:51:24 -0400

    Pulled Swank and Combinatorics deps into the project file.

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject clojure-poker "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+		 [org.clojure/math.combinatorics "0.0.2"]]
+  :plugins [[lein-swank "1.4.4"]])
