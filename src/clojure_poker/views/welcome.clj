96e8b3b85aa3300fb9ff7275bc5f1afae24e2538
Michael Drogalis <mjd3089@rit.edu>
2012-05-29 14:13:26 -0400

    Test case button.

--- src/clojure_poker/views/welcome.clj
+++ src/clojure_poker/views/welcome.clj
@@ -1,7 +1,8 @@
 (ns clojure-poker.views.welcome
   (:require [clojure-poker.views.common :as common])
   (:use [clojure-poker.models.poker])
-  (:use [noir.core :only [defpage]]))
+  (:use [noir.core :only [defpage]])
+  (:use [hiccup.element]))
 
 (defpage "/" []
   (let [shuffled-deck (shuffle deck)
@@ -12,4 +13,5 @@
      (common/hand player-1)
      [:h3 "Player 2"]
      (common/hand player-2)
-     [:h3 "Winner is " (winner-of { :player-1 player-1 :player-2 player-2} )])))
+     [:h3 "Winner is " (winner-of { :player-1 player-1 :player-2 player-2} )]
+     (link-to {:class "big blue nice radius button"} "#" "Generate test case"))))
