f124fc83091ea7d591e5aeb138027bf7cbf0a9c0
Michael Drogalis <mjd3089@rit.edu>
2012-05-29 16:14:59 -0400

    Proper indentation.

--- src/clojure_poker/views/welcome.clj
+++ src/clojure_poker/views/welcome.clj
@@ -23,31 +23,30 @@
 	player-2 (take 5 (drop 5 shuffled-deck))]
     (common/layout
      [:div.row
-      [:div.six.columns
+      [:div.twelve.columns
        (player-row "Player 1" player-1)
-       (player-row "Player 2" player-2)]
-      [:div.six.columns
-       [:div.row
+       (player-row "Player 2" player-2)]]
+     [:div.row
+      [:div.twelve.columns
+       [:div.panel
+	[:h4 "Winner is " (winner-of { :player-1 player-1 :player-2 player-2})]]
+       (link-to {:class "big blue nice radius button"} "javascript:generateTestCase();" "Generate test case")]]
+     [:div.row
 	[:div.twelve.columns
-	 [:div.panel
-	[:h3 "Winner is " (winner-of { :player-1 player-1 :player-2 player-2} )]
-	(link-to {:class "big blue nice radius button"} "javascript:generateTestCase();" "Generate test case")]]]
-       [:div.row
-	[:div.twelve.columns
-	 [:pre#test-case]]]]])))
+	 [:pre#test-case]]])))
 
 (defpage "/to-test-case" []
   (escape-html
    "(deftest bug-report-test-case
       (let
-	  [players {:player-1 [(card-of :ace :hearts)
-			       (card-of :king :spades)
-			       (card-of :queen :hearts)
-			       (card-of :jack :diamonds)
-			       (card-of :nine :clubs)]
-		    :player-2  [(card-of :two :hearts)
-				(card-of :two :clubs)
-				(card-of :king :hearts)
-				(card-of :jack :diamonds)
-				(card-of :nine :hearts)]}]
-	(is (= #{:player-2} (winner-of players)))))"))
+        [players {:player-1 [(card-of :ace :hearts)
+                             (card-of :king :spades)
+                             (card-of :queen :hearts)
+                             (card-of :jack :diamonds)
+                             (card-of :nine :clubs)]
+                  :player-2 [(card-of :two :hearts)
+                             (card-of :two :clubs)
+                             (card-of :king :hearts)
+                             (card-of :jack :diamonds)
+                             (card-of :nine :hearts)]}]
+        (is (= #{:player-2} (winner-of players)))))"))
