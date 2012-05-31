fd61cfdb497e26e60d0a8e39e1ccc40cfe1c0ae3
Mike Drogalis <mjd3089@rit.edu>
2012-05-31 12:12:44 -0400

    Got the table up.

--- src/clojure_poker/views/welcome.clj
+++ src/clojure_poker/views/welcome.clj
@@ -18,27 +18,6 @@
        (common/hand cards)]]]]])
 
 (defpage "/" []
-  (let [shuffled-deck (shuffle deck)
-	player-1 (take 5 shuffled-deck)
-	player-2 (take 5 (drop 5 shuffled-deck))]
-    (common/layout
-     [:div.row
-      [:div.twelve.columns
-       (player-row "Player 1" player-1)
-       (player-row "Player 2" player-2)]]
-     [:div.row
-      [:div.twelve.columns
-       [:div.panel
-	[:h4 "Winner is " (winner-of { :player-1 player-1 :player-2 player-2})]]
-       (link-to {:class "big blue nice radius button"} "javascript:generateTestCase();" "Generate test case")]]
-     [:div.row
-	[:div.twelve.columns
-	 [:pre#test-case
-	  (escape-html
-	   (str
-	    "(deftest bug-report-test-case\n"
-             "  (let\n"
-               "    [players {:player-1 " (vector player-1) "\n"
-                          "              :player-2 " (vector player-2) "}]\n"
-        "    (is (= " (winner-of { :player-1 player-1 :player-2 player-2}) " (winner-of players)))))" ))]]])))
-
+  (common/layout
+   [:div#poker-table.row
+    [:div.twelve.columns]]))
