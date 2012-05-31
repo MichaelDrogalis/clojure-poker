1edce4da48cb84cd0f857fb50b4d43ffc9f59270
Mike Drogalis <mjd3089@rit.edu>
2012-05-31 13:20:58 -0400

    Dealing 2 cards.

--- resources/public/js/app.js
+++ resources/public/js/app.js
@@ -1,5 +1,7 @@
 /* Foundation v2.2.1 http://foundation.zurb.com */
 
+var delay = 300;
+
 function generateTestCase() {
     $("#test-case").show().addClass("brush: clojure");
     SyntaxHighlighter.highlight();
@@ -7,16 +9,32 @@ function generateTestCase() {
 
 $(document).ready(function() {
     $("#card-1").animate({
-	left: '+=250', top: '+=50', duration: 300
-    }).promise().done(function() {
+	left: '+=250', top: '+=50'
+    }, delay).promise().done(function() {
         $("#card-2").animate({
-            left: '+=250', top: '+=300', duration: 300
-        }).promise().done(function() {
+            left: '+=250', top: '+=300'
+        }, delay).promise().done(function() {
             $("#card-3").animate({
-                left: '-=250', top: '+=300', duration: 300
-            }).promise().done(function() {
+                left: '-=250', top: '+=300'
+            }, delay).promise().done(function() {
 		$("#card-4").animate({
- 	            left: '-=250', top: '+=50', duration: 300
+ 	            left: '-=250', top: '+=50'
+                }, delay).promise().done(function() {
+		    $("#card-5").animate({
+ 	                left: '+=275', top: '+=50'
+                    }, delay).promise().done(function() {
+			$("#card-6").animate({
+                            left: '+=275', top: '+=300'
+			}, delay).promise().done(function() {
+                	    $("#card-7").animate({
+                                left: '-=225', top: '+=300'
+                            }, delay).promise().done(function() {
+			        $("#card-8").animate({
+                                    left: '-=225', top: '+=50'
+                                }, delay);
+                            });
+			});
+		    });
                 });
             });
         });
